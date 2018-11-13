05e9fdd851ffd6784cbb1415c42ecdecce749c6c
Soyeon65 <43192552+Soyeon65@users.noreply.github.com>
2018-11-13 18:52:07 -0500

    Add files via upload

---
+++ scripttwo.js
@@ -0,0 +1,19 @@
+/*var todayDate = new Date();
+
+var todayDay = todayDate.getDate();
+var todayMonth = todayDate.getMonth()+1;
+var todayYear = todayDate.getFullYear();
+
+var formatedCode = todayDate.getDate() + "/" + todayMonth + "/" + todayDate.getFullYear();
+document.getElementById("para").innerHTML = formatedCode; */
+
+/*var stringNum = 8;
+var output = Number(stringNum) + "2";
+document.getElementById("para").innerHTML = output;*/
+
+/*var myAlert = alert("This is an alert!");
+var myConfirm = confirm("This is an confirm!");
+var myPrompt = prompt("This is an prompt!");
+console.log(myAlert);
+console.log(myConfirm);
+console.log(myPrompt);*/
